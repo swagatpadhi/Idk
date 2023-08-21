a599581423c226317330547878dd899e692fdf4f
Swagat <swagatpadhi03@gmail.com>
2023-08-21 19:00:04 +0530

    second commit

---
+++ src/LetmeSee.java
@@ -0,0 +1,10 @@
+public class LetmeSee {
+    public static void main(String[] args){
+        System.out.println("welcome Swagat , doing good");
+        int i = 1;
+        while(i < 10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
